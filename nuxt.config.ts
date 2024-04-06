4963640a679f42bd89a42f0dc5c5609db241f1f8
林庭楷 <hellolinpeter@gmail.com>
2024-04-06 22:58:15 +0800

    change config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   //...
-  routeRules: {
-    '/examples/*': { redirect: '/redirect-route' },
-    '/modify-headers-route': { headers: { 'x-magic-of': 'nuxt and vercel' } },
-    // Enables client-side rendering
-    '/spa': { ssr: false },
-  },
+
   build: {
     transpile: ['vuetify'],
   },
